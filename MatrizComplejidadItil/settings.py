1e15ea87297f36add8ca56ff0e289621b379a4b1
Marcelo <snaker.extreme.15@gmail.com>
2022-05-10 20:44:10 -0400

    Se modifica ruta de static files

--- MatrizComplejidadItil/settings.py
+++ MatrizComplejidadItil/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/image")
+
+MEDIA_URL = "/image/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
